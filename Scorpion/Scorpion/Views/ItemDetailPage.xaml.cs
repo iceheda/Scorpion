45e507995f6c69400a08de45595246b981087a34
Azgeda <neis.stalker@mail.ru>
2024-01-20 16:53:39 +0400

    Добавьте файлы проекта.

---
+++ Scorpion/Scorpion/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Scorpion.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Scorpion.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
